43a88b44e7b6699db7ca3f2f0648fd0d1959ccfe
DFVC9914 <cuichao_970104@163.com>
2020-12-08 20:16:19 +0000

    1

--- Main_Game.py
+++ Main_Game.py
@@ -149,12 +149,12 @@ def Game_Main(P_Screen_Width,P_Screen_Height,P_Highest_y,P_Lowest_y,P_Background
     Game_Over = ""  
     # The images of the game
     Role_Image_Action = ["Images/Roles/Role_Run_1.png","Images/Roles/Role_Run_2.png","Images/Roles/Role_Jump.png"]
-<<<<<<< HEAD
+
     Barriers_Images = P_Barriers_Images
     
-=======
+
     Barriers_Images = [["Images/Barriers/Barrier_Bottom_1.png","Images/Barriers/Barrier_Bottom_1_2.png"],["Images/Barriers/Barrier_Bottom_2_1.png","Images/Barriers/Barrier_Bottom_2_2.png"],["Images/Barriers/Barrier_Top_1_1.png","Images/Barriers/Barrier_Top_1_2.gif"],["Images/Barriers/Barrier_Top_2_1.png","Images/Barriers/Barrier_Top_2_2.png"]]
->>>>>>> 19251c8b549140c99be688831fe1d70c04f9e8bc
+
     Golds_Images = [["Images/Barriers/Gold_1.png"]] 
     Background = P_Background
     Game_Over_Image = pygame.image.load("Images/Game_Over.png").convert_alpha()
