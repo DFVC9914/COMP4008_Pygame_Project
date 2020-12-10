1b1cbf8549f90bfa020610298f3cd73397d57145
NANDI GUO <44260801+907300985@users.noreply.github.com>
2020-12-10 00:44:40 +0000

    1

--- Congratulation.py
+++ Congratulation.py
@@ -4,7 +4,8 @@ Created on Fri Dec 4 22:09:30 2020
 
 @author: NANDI GUO
 """
-import pygame,os
+import pygame,os,Game_Modes,Start_Screen
+
 
 Orange = (119,0,255)
 White = (255,255,255)
@@ -16,16 +17,39 @@ def Con_Screen():
     global screen,font
     pygame.init()
     screen = pygame.display.set_mode((Width, Height))  
-    pygame.display.set_caption("Game Modes")
+    pygame.display.set_caption("Congratulation!")
     background = pygame.image.load("Images/Backgrounds/Congratulation_1.png")
     screen.blit(background, (0,0))
     cong = pygame.mixer.Sound("Sounds/happy.mp3")
     cong.play()
     
+    b1x,b1y=670,400
+    back_button = Start_Screen.Button('Back', Orange, b1x, b1y)
+    back_button.display()
+    pygame.display.update()
+    
     while True:
+        if not back_button.check_click(pygame.mouse.get_pos()):                
+            pygame.mixer.music.play()
+            
+        if back_button.check_click(pygame.mouse.get_pos()):              
+            back_button = Start_Screen.Button('Back', Blue, b1x, b1y)                        
+        else:            
+            back_button = Start_Screen.Button('Back', Orange, b1x, b1y)
+
+        back_button.display()
         pygame.display.update()
+        
         for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         os._exit(0)
 
+        if pygame.mouse.get_pressed()[0]:            
+                         
+            if back_button.check_click(pygame.mouse.get_pos()):
+                cong.stop()
+                Start_Screen.Firstscreen()
+                pygame.quit()                
+                os._exit(0)
+                break
